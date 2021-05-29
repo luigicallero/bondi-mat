299447dca13030eb4265e15ed0bd4e9da3f07f36
luigicallero <58836287+luigicallero@users.noreply.github.com>
2021-05-29 12:26:29 -0300

    uploading version from May 29 2021

---
+++ truffle-config.js
@@ -0,0 +1,45 @@
+require('babel-register')
+require('babel-polyfill')
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+
+require('dotenv').config()
+const mnemonic = process.env.MNEMONIC
+const url = process.env.RPC_URL
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    live: {
+      provider: () => {
+        return new HDWalletProvider(process.env.MNEMONIC, process.env.KOVAN_RPC_URL)
+      },
+      network_id: '*',
+      // ~~Necessary due to https://github.com/trufflesuite/truffle/issues/1971~~
+      // Necessary due to https://github.com/trufflesuite/truffle/issues/3008
+      skipDryRun: true,
+    },
+    kovan: {
+      provider: () => {
+        return new HDWalletProvider(mnemonic, url)
+      },
+      network_id: '42',
+      skipDryRun: true
+    },
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+      evmVersion: "petersburg",
+      version: "0.6.6"
+    }
+  }
+}
